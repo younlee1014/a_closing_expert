f0bde2a9c558ccf2c3f6737e4bce538f3e34ac3f
yyksin@naver.com <did6811>
2020-01-28 17:23:48 +0900

    @ParameterizedTest @ValueSource 적용 및 테스트 파라메터 추가

--- src/main/java/calculate/CalculateMain.java
+++ src/main/java/calculate/CalculateMain.java
@@ -46,7 +46,7 @@ public class CalculateMain {
     }
 
     public static String[] inputSplit(String input){
-        if(input == null || input.length() == 0)
+        if(input == null || input.trim().length() == 0)
             throw new IllegalArgumentException();
 
         return input.split(" ");
